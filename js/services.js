0362122e84c2ee992b8c82aa61c79be677c39454
silphashree <roshh1@gmail.com>
2016-09-13 21:28:11 -0400

    Added my first JS

---
+++ js/services.js
@@ -0,0 +1,37 @@
+var printing = document.getElementById("printing");
+var lamination = document.getElementById("lamination");
+var shooting = document.getElementById("shooting");
+var albums = document.getElementById("albums");
+var dimond = document.getElementById("dimond");
+var silver = document.getElementById("silver");
+
+var other_services = document.getElementById("other-services");
+var txt_printing = document.getElementById("txt-printing");
+var txt_lamination = document.getElementById("txt-lamination");
+var txt_shooting = document.getElementById("txt-shooting");
+var txt_albums = document.getElementById("txt-albums");
+var txt_dimond = document.getElementById("txt-dimond");
+var txt_silver = document.getElementById("txt-silver");
+
+printing.addEventListener("click", pickLink);
+lamination.addEventListener("click", pickLink);
+shooting.addEventListener("click", pickLink);
+albums.addEventListener("click", pickLink);
+dimond.addEventListener("click", pickLink);
+silver.addEventListener("click", pickLink);
+
+function pickLink () {
+	var allContent = [other_services, txt_printing, txt_lamination, txt_shooting, txt_albums, txt_dimond, txt_silver];	
+	for (var i = 0; i < allContent.length; i++) {
+		allContent[i].className = "hide";
+	}
+	var pickId = this.attributes["data-content"].value;
+	var pick = document.getElementById(pickId);
+	
+	if ( pick.className === "hide") {
+		pick.className = "";
+	}
+	else {
+		pick.className = "hide";
+	}
+}
